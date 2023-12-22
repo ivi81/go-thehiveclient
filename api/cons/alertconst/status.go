f885873f8cf28f69a4536e3721b35932c40f4c83
Илья Ипполитов <i.ippolitov@cloud.gcm>
2023-12-22 16:27:16 +0200

    UDATE alert const defintion

--- api/cons/alertconst/status.go
+++ api/cons/alertconst/status.go
@@ -1,12 +1,7 @@
 package alertconst
 
-import (
-	"strings"
-
-	"gitlab.cloud.gcm/i.ippolitov/go-thehiveclient/api/cons/common"
-)
-
 //go:generate stringer -type=Status
+//go:generate enummethods -type=Status
 type Status int
 
 const (
@@ -15,40 +10,3 @@ const (
 	Ignored
 	Imported
 )
-
-func (m Status) IsValid() bool {
-
-	switch m {
-	case
-		New,
-		Updated,
-		Ignored,
-		Imported:
-
-		return true
-	}
-	return false
-}
-
-//SetValue конвертация строки в значение типа
-func (m *Status) SetValue(s string) bool {
-	i := strings.Index(_Status_name, s)
-	if i != -1 {
-
-		for index, v := range _Status_index {
-			if i-int(v) == 0 {
-				*m = Status(index)
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (m *Status) MarshalJSON() ([]byte, error) {
-	return common.MarshalConstantJSON(m)
-}
-
-func (m *Status) UnmarshalJSON(data []byte) error {
-	return common.UnmarshalConstantJSON(m, data)
-}
